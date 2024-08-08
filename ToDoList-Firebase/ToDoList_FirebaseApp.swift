144cd3fedd015e2bce80a1c070e42ca20e866601
Engulfedleader2 <engulfedleader@gmail.com>
2024-08-08 16:25:33 -0400

    Initial Commit

---
+++ ToDoList-Firebase/ToDoList_FirebaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoList_FirebaseApp.swift
+//  ToDoList-Firebase
+//
+//  Created by Israel on 8/8/24.
+//
+
+import SwiftUI
+
+@main
+struct ToDoList_FirebaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
